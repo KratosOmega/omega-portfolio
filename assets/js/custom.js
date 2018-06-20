3ae4b3d879b8da40ba9615c0d9f8a252e8fd41f1
Xin Li <KratosOmega@iCloud.com>
2018-06-20 15:23:42 -0400

    update resume page & update resume pdf & update blog list layout

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,61 +12,60 @@ $(document).ready(function(){
 
     /* ############################################################# */
 
-  // ********************************************************************************************************************* initiate the body to behide 
-  $(".default-hide").hide();
-  $("#main").show();
-
-  // ********************************************************************************************************************* click to show the page
-  $("#showMain").click(function(){
+    // ********************************************************************************************************************* initiate the body to behide 
     $(".default-hide").hide();
     $("#main").show();
-  });
-  //
-  $("#showAboutMe").click(function(){
-    $(".default-hide").hide();
-    $("#aboutMe").show();
-  });
-  //
-  $("#showResume").click(function(){
-    $(".default-hide").hide();
-    $("#resume").show();
-  });
-  //
-  $("#showProject").click(function(){
-    $(".default-hide").hide();
-    $("#project").show();
-  });
-  //
-  $("#showBlog").click(function(){
-    $(".default-hide").hide();
-    $("#blog").show();
-  });
-  //
-  $("#showContact").click(function(){
-    $(".default-hide").hide();
-    $("#contact").show();
-  });
-  //
 
-  // ********************************************************************************************************************* click to switch skill set
-  $(".default-skill-hide").hide();
-  $("#backend").show();
-  //
-  $("#showBackend").click(function(){
-    $(".default-skill-hide").hide();
-    $("#backend").show();
-  });
-  //
-  $("#showFrontend").click(function(){
-    $(".default-skill-hide").hide();
-    $("#frontend").show();
-  });
-  //
-  $("#showDevelopment").click(function(){
-    $(".default-skill-hide").hide();
-    $("#development").show();
-  });
-  //
+    // ********************************************************************************************************************* click to show the page
+    $("#showMain").click(function(){
+        $(".default-hide").hide();
+        $("#main").show();
+    });
+    //
+    $("#showAboutMe").click(function(){
+        $(".default-hide").hide();
+        $("#aboutMe").show();
+    });
+    //
+    $("#showResume").click(function(){
+        $(".default-hide").hide();
+        $("#resume").show();
+    });
+    //
+    $("#showProject").click(function(){
+        $(".default-hide").hide();
+        $("#project").show();
+    });
+    //
+    $("#showBlog").click(function(){
+        $(".default-hide").hide();
+        $("#blog").show();
+    });
+    //
+    $("#showContact").click(function(){
+        $(".default-hide").hide();
+        $("#contact").show();
+    });
+    //
+
+    // ********************************************************************************************************************* click to switch exp & skill set
+    $(".default-exp-hide").hide();
+    $("#research").show();
+    //
+    $("#showResearch").click(function(){
+        $(".default-exp-hide").hide();
+        $("#research").show();
+        //document.getElementById("#showResearch").className = "btn btn-default active";
+        //document.getElementById("#showWebDev").className = "btn btn-default";
+    });
+    //
+    $("#showWebDev").click(function(){
+        $(".default-exp-hide").hide();
+        $("#webDev").show();
+        //document.getElementById("#showWebDev").className = "btn btn-default active";
+        //document.getElementById("#showResearch").className = "btn btn-default";
+    });
+    //
 });
 
 
